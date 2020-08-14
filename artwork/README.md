0451ef1617b1ba68babc6a5efa604410b276dd35
antony-jr <antonyjr@protonmail.com>
2020-08-14 16:06:36 +0530

    make updates row in index

---
+++ artwork/README.md
@@ -0,0 +1,5 @@
+# Website Artwork Design
+
+Make sure to copy contents of ```fonts/``` directory to ```~/.fonts``` directory to
+make gimp find the required fonts for the design.
+
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import matter from 'gray-matter'
+import ReactMarkdown from 'react-markdown'
+
 import Fade from 'react-reveal/Fade';
 import Pulse from 'react-reveal/Pulse';
 import Flash from 'react-reveal/Flash';
@@ -17,6 +20,8 @@ import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Image from 'react-bootstrap/Image'
+import CardColumns from 'react-bootstrap/CardColumns';
+import Card from 'react-bootstrap/Card';
 
 import { SocialIcon } from 'react-social-icons';
 
@@ -26,6 +31,10 @@ const SIcon = styled(SocialIcon)`
 
 const WrapperCol = styled(Col)`
 	height: 400px;
+
+	@media (max-width: 600px) {
+		height: 500px;
+	}
 
 	@media (max-width: 375px) {
 		height: 350px;
@@ -40,7 +49,44 @@ const WrapperCol = styled(Col)`
 	}
 `;
 
-function Index() {
+function BlogCard(props) {
+	return (
+	<Card 
+	  style={{margin: '10px'}}
+		bg={props.dark ? "dark" : ""} text={props.dark ? "white": "black"}
+		className={"text-center p-" + (typeof props.cardSize != 'undefined' ? props.cardSize : '2')}>
+		{props.image && <Card.Img variant="top" src={props.image}/>}
+	
+		<blockquote className="blockquote mb-0 card-body">
+	    <Card.Title>
+		    <Typography type={props.titleSize}>
+			    {props.title}
+		    </Typography>
+	    </Card.Title>
+	    <Typography type='p'>
+		    {props.description}
+	    </Typography>
+      <footer className="blockquote-footer">
+        <small className="text-muted">
+		<Typography type='h6'>
+		{props.author}
+		</Typography>
+	</small>
+      </footer>
+    </blockquote>
+	  <Card.Footer>
+		  <small>
+		<Typography type='h6'>
+			<b>{props.date}</b>
+		</Typography>
+		</small>
+	  </Card.Footer>
+  </Card>
+	
+	);
+}
+
+function Index(props) {
 	return (
 		<React.Fragment>
 			<Container style={{marginTop: '40px',}} fluid='lg'>
@@ -89,7 +135,7 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend nisi in ia
 				</Zoom>
 
 				<Row style={{justifyContent: 'center', alignItems: 'center'}}>
-					<Col style={{height: '500px', textAlign: 'center', alignItems: 'center', justifyContent: 'center'}}>
+					<Col style={{textAlign: 'center', alignItems: 'center', justifyContent: 'center'}}>
 					<Zoom>
 						<br/>
 						<br/>
@@ -100,6 +146,23 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend nisi in ia
 
 					<br/>
 					</Col>
+				</Row>
+				<Row style={{justifyContent: 'center', alignItems: 'center'}}>
+					<Fade>
+					<CardColumns>
+						{props.posts.map((key, index) =>
+						<BlogCard 
+							dark={key.dark}
+							title={key.title}
+							description={key.description}
+							date={key.date}
+							author={key.author}
+							titleSize="h3"
+							cardSize={key.cardSize}
+						/>)
+						}
+					</CardColumns>
+					</Fade>
 				</Row>
 				<Row style={{justifyContent: 'center', alignItems: 'center'}}>
 					<Col style={{textAlign: 'center', alignItems: 'center', justifyContent: 'center'}}>
@@ -145,5 +208,88 @@ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend nisi in ia
 		</React.Fragment>
 	);
 }
+
+
+export async function getStaticProps() {
+  //get posts & context from folder
+  const posts = (context => {
+    const keys = context.keys()
+    const values = keys.map(context)
+
+    const data = keys.map((key, index) => {
+      // Create slug from filename
+      const slug = key
+        .replace(/^.*[\\\/]/, '')
+        .split('.')
+        .slice(0, -1)
+        .join('.')
+      const value = values[index]
+      // Parse yaml metadata & markdownbody in document
+      const document = matter(value.default)
+      return {
+	      ...document.data,
+	      slug
+      }
+    })
+    return data
+  })(require.context('../posts', true, /\.md$/))
+
+  function validFile(entry) {
+	  	var slugName = entry.slug;
+		var r = slugName.split('-');
+	  	if(r.length < 3) {
+			return false;
+		}
+
+	  	// Check if it's a valid ISO Date.
+		var year = Number(r[0]),
+		    month = Number(r[1]),
+		    day = Number(r[2]);
+
+		if(day > 31 || month > 12) {
+			return false;
+		}
+
+		return true;
+  }
+ 
+  function getDate(entry) {
+	  var slugName = entry.slug;
+	  var r = slugName.split('-');
+	  if(r.length < 3) {
+			return new Date("Invalid");
+	  }
+	  var year = Number(r[0]),
+	      month = Number(r[1]),
+	      day = Number(r[2]);
+	  return new Date(year + '-' + month + '-' + day);
+  }
+
+  var workingPosts = posts.filter(validFile);
+  workingPosts.sort(function(a,b) {
+	const bd = new Date(b.slug);
+	const ad = new Date(a.slug);
+	return bd - ad;
+  });
+  
+
+  var updates = workingPosts.slice(0, 8); // Take up first 8 entries
+  for(let n = 0; n < updates.length; ++n) {
+	  var cardSize = 5 - n;
+	  if(cardSize < 2) {
+		  cardSize = 2;
+	  }
+	  updates[n].cardSize = cardSize.toString();
+	  updates[n].dark = Math.random() >= 0.5;
+	  updates[n].date = getDate(updates[n]).toDateString();
+  }
+
+  return {
+    props: {
+      posts: updates,
+    },
+  }
+}
+
 
 export default Index;
